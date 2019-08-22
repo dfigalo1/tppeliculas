8a730b6701496b14c05579975b2d78f360d36045
GracielaLG <graceland@MacBook-Pro-de-Graciela.local>
2019-08-22 16:15:58 -0300

    api key

--- trabajo4.js
+++ trabajo4.js
@@ -1,29 +1,6 @@
-//
-// categoria Popular//
 
-//async function funcionAsincronica(){
-   // try//{
-   //     let response = await initialize
-    //}
-//} 
-//const initialize = () => fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${0b63f51adbe9c3ff14acc1ab92206b7d}`
-//)
-  //.then(response => response.json())
-  //.then(response => funct//ionName(response)//)
-
-async function Data () {
-  try{
-    let dataApi = await fetch
-    ("https://api.themoviedb.org/3/movie/popular?api_key=0b63f51adbe9c3ff14acc1ab92206b7d&page=${paginaActual}")
-  .then(response => await response.json(dataApi))
-  .then(res => {
-      
-  })
-  }catch (e) {
-    console.log (e)
-  }finally{
-    console.log ('sin resultados')
-  }
-}
-
-Data()
+ const apiKey = '0b63f51adbe9c3ff14acc1ab92206b7d'
+ const paginaActual = '1'
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${paginaActual}`)
+.then(response => response.json())
+.then(res => console.log(res))
