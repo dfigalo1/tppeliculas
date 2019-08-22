3ad1366f55619fb1393f340f92b997cfcbb65511
GracielaLG <graceland@MacBook-Pro-de-Graciela.local>
2019-08-22 16:41:02 -0300

    customFetch

--- trabajo4.js
+++ trabajo4.js
@@ -1,6 +1,9 @@
 
- const apiKey = '0b63f51adbe9c3ff14acc1ab92206b7d'
- const paginaActual = '1'
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${paginaActual}`)
-.then(response => response.json())
-.then(res => console.log(res))
+const customFetch = (apiKey) => {
+  const endPoint = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+  return fetch (endPoint)
+   .then (response => response.json())
+   .then (res => console.log(res))
+}
+
+customFetch('0b63f51adbe9c3ff14acc1ab92206b7d')
