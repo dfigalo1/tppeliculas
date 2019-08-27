4bdb1633c1c2ead68bdde4f6f44ce09206fc0063
dfigalo1 <daianafigalo@gmail.com>
2019-08-27 16:42:28 -0300

    modal y placeholder

---
+++ js/prueba.js
@@ -0,0 +1,113 @@
+ // FUNCIONES REUTILIZABLES maybe
+ 
+ let apiKey = '0b63f51adbe9c3ff14acc1ab92206b7d';
+ let categories = ['popular', 'top_rated', 'upcoming', 'now_playing']
+ 
+ const customFetch = (apiKey, method, payload = '') => {
+  const endPoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`
+  let options = {
+    method: method,
+    headers: {'content-type': 'application/json'}
+  }
+  if(method !== 'GET' && payload) options.body = payload
+  return fetch (endPoint, options)
+   .then (response => response.json())
+   .then (res => (res))
+}
+
+ 
+const karin = (elem, classname) => {
+  let thing = document.createElement(elem)
+  thing.classList.add(classname)
+  return thing
+}
+
+  // SEARCH INPUT
+  let lastSearch;
+
+  const handleSearch = () => {
+    let searchText = event.target.value;
+    if ( searchText.length >= 3 || event.keyCode === 13 && searchText !== lastSearch){
+        lastSearch = searchText
+        customFetch(`&query=${searchText}`, 'GET')
+        //preguntar en clase por que no funciona con la /popular ? fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchText}`)
+        //.then( res => res.json())
+        .then ( res => DropDownResults(res.results));
+    } 
+
+  };
+
+  const DropDownResults = (movies) => { 
+    const container = document.getElementById('results');
+    container.innerHTML = '';
+    movies.forEach((movie) => {
+      let searchResults = karin('a', 'resultName');
+      searchResults.href = '#';
+      let title = movie.title === movie.origila_title ? movie.title:`${movie.title} (${movie.original_title})`;
+      searchResults.innerText = title;
+      container.appendChild(searchResults);
+
+    })
+  };
+   
+  //  HOME PAGE
+
+  const homeData = (category) => {
+    return fetch (`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
+    .then (response => response.json())
+    .then(res => {
+      for(i=0; i<5;i++){
+        printPopularHome(res.results[i], category)
+      }
+    })
+  }
+
+  const printPopularHome = ({title, id,  poster_path}, idContainer) => {
+    const ImageContainer = document.getElementById(idContainer);
+      let div = karin('div' ,"imgContainer");
+      let a = karin('a', 'imageLink');
+      a.href = '#';
+      let imageResults = karin('img', 'image');
+      imageResults.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+      let titleResults = karin('p', 'imagetitle');
+      titleResults.innerText = title;
+    
+      ImageContainer.appendChild(div);
+      div.appendChild(a);
+      a.appendChild(imageResults);
+      a.appendChild(titleResults); 
+  
+    };
+
+  const initialize = () => {
+    categories.forEach(e => homeData(e))
+}
+
+  // LOAD MORE
+
+  let currentPage = 'currentPage + 1'
+
+
+  
+// MODAL
+let modal = document.getElementById('miModal');
+let flex = document.getElementById('flex');
+let boxes = document.getElementById('open');
+let close = document.getElementById('close');
+
+
+boxes.addEventListener('click',function(){
+    modal.style.display = 'block' ;
+});
+
+
+close.addEventListener('click',function(){
+    modal.styles.display = 'none';
+});
+
+
+window.addEventListener('click',function(){
+if(e.target == flex){
+    modal.style.display = 'none';
+}
+});
--- js/trabajo4.js
+++ js/trabajo4.js
@@ -39,6 +39,158 @@ if(e.target == flex){
 
 
 
-// Apikey
-// 3e597a68977c4cb049bc74d03c2e7ea7
+const customFetch = (apiKey, method, payload = '') => {
+  const endPoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`
+  let options = {
+    method: method,
+    headers: {'content-type': 'application/json'}
+  }
+  if(method !== 'GET' && payload) options.body = payload
+  return fetch (endPoint, options)
+   .then (response => response.json())
+   .then (res => res)
+}
+
+customFetch(`&query=${searchText}`, 'GET')
+let categories = 'popular'
+
+const customFetchCategories = (apiKey, method, payload = '') => {
+  const endPoint = `https://api.themoviedb.org/3/movie/${categories}?api_key=${apiKey}`
+  let options = {
+    method: method,
+    headers: {'content-type': 'application/json'}
+  }
+  if(method !== 'GET' && payload) options.body = payload
+  return fetch (endPoint, options)
+   .then (response => response.json())
+   .then (res => (res))
+}
+
+
+
+customFetch(`/popular?api_key=${apiKey}`, 'GET')
+  .then (res => data = res )
+  //usando en customeFetch
+  `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`
+
+  //la que nos dan en tp4 para la busqueda
+  `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${textoBusqueda}&page=${paginaActual}`
+
+//la que usamos para busqueda y que funciona
+   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchText}`)
+    .then( res => res.json())
+
+let apiKey = '0b63f51adbe9c3ff14acc1ab92206b7d'
+let currentPage = 'currentPage + 1'
+
+
+// la que nos da el TP4 y no usamos pero es para home
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+// la que nos da el TP4 y no usamos pero es para Listado por categoría
+fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=${paginaActual}`)
+ .then (response => response.json())
+ .then (res => data = res) 
+
+
+
+
+
+const karin = (elem, classname) => {
+  let thing = document.createElement(elem)
+  thing.classList.add(classname)
+  return thing
+}
+
+const homeMovies = movies => {
+
+
+  movies.forEach(e => {
+   let image = karin('img', 'image', e.poster_path)
+   image.src = e.poster_path
+   let p = karin('p', 'title')
+   p.innerText = title
+
+   container.appendchild(img)
+   img.appendchild(p)
+  
+  });
+}
+
+
+
+
+  const handleSearch = () => {
+    console.log(event.target.value);
+
+  };
+  
+const printResults = (movies) => {
+  const container =  document.getElementById("results");
+  container.innerHTML = '';
+  
+  movies.forEach(mov) => {
+    let movie = document.createElement('a');
+    let title = mov.title === mov.original_title ? mov.title : `${mov.title}(${mov.original_title})
+    movie.innerText = title;
+  container.appendChild(movie);
+  });
+};
+
+const openmodal = ()
+
+/// const printcategoriesResults = (movies
+)
+
+
+//  HOME PAGE
+
+  let categories = ['/top_rated','/popular','/upcoming','/now_playing']
+  const homeData = () =>
+  customFetchCategories(`?api_key=${apiKey}`, 'GET')
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(res => {
+      for(i=0; i<5;i++){
+        (res.results[i])
+      }
+    })
+  
+  
+ 
+
+
+const printPopularHome = (data) => {
+  const ImageContainer = document.getElementById('imgContainer');
+    let movieTitle = document.createElement('img');
+    movieTitle.className = "image";
+    movieTitle.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+    ImageContainer.appendChild(movieTitle);
+
+  };
+
+  const customFetchCategories = (apiKey, method, payload = '') => {
+    const endPoint = `https://api.themoviedb.org/3/movie/${categories}?api_key=${apiKey}`
+    let options = {
+      method: method,
+      headers: {'content-type': 'application/json'}
+    }
+    if(method !== 'GET' && payload) options.body = payload
+    return fetch (endPoint, options)
+     .then (response => response.json())
+     .then (res => (res))
+  }
+
+  const printPopularHome = (popularMovies) => {
+    const ImageContainer = document.getElementById('imgContainer');
+    popularMovies.forEach((popularMovie) => {
+      let searchResults = karin('img', 'image');
+      let title = popularMovies.poster_path;
+      popularMovie.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+      
+      searchResults.innerText = title;
+      ImageContainer.appendChild(searchResults);
+  
+    })
+    };
+  
 
