a8bbd89bca907f89a82d2a54e633e5ada9ecffbb
GracielaLG <graceland@MacBook-Pro-de-Graciela.local>
2019-08-29 17:23:43 -0300

    cambios en el html para categorias

--- js/prueba.js
+++ js/prueba.js
@@ -1,4 +1,4 @@
-/ FUNCIONES REUTILIZABLES maybe
+// FUNCIONES REUTILIZABLES
  
  let apiKey = '0b63f51adbe9c3ff14acc1ab92206b7d';
  let categories = ['popular', 'top_rated', 'upcoming', 'now_playing']
@@ -43,6 +43,11 @@ const karin = (elem, classname) => {
     movies.forEach((movie) => {
       let searchResults = karin('a', 'resultName');
       searchResults.href = '#';
+      
+      searchResults.onclick = () => {
+        modalData(movie.id)
+  
+    };
       let title = movie.title === movie.origila_title ? movie.title:`${movie.title} (${movie.original_title})`;
       searchResults.innerText = title;
       container.appendChild(searchResults);
@@ -57,17 +62,18 @@ const karin = (elem, classname) => {
     .then (response => response.json())
     .then(res => {
       for(i=0; i<5;i++){
-        printPopularHome(res.results[i], category)
+        printHome(res.results[i], category)
       }
     })
   }
 
-  const printPopularHome = ({title, id,  poster_path}, idContainer) => {
+  const printHome = ({title, id,  poster_path}, idContainer) => {
     const ImageContainer = document.getElementById(idContainer);
       let div = karin('div' ,"imgContainer");
-      let a = karin('a', 'imageLink');
-      a.href = '#';
-     
+      
+      let a = karin('a', 'imageLink');  
+      a.href = '#'; 
+
       let imageResults = karin('img', 'image');
       imageResults.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
       let titleResults = karin('p', 'imagetitle');
@@ -84,32 +90,85 @@ const karin = (elem, classname) => {
     categories.forEach(e => homeData(e))
 }
 
+//categoria popular 
+
+
+( function name(d) {
+  let tabs = d.querySelectorAll('.tabs_item');
+  let panels = d.querySelectorAll('.categories_items');
+  d.getElementById('tabs').addEventListener('click', e => {
+    if(e.target.classList.contains ('tabs_item')){
+      let i = (tabs.indexOf(e.target));
+      tabs.map(tab => tab.classList.remove('active'))
+      tabs[i].classList.add('active');
+      panels.map(panel => panel.classList.remove('active'))
+      panels[i].classList.add('active');
+
+    }
+
+  } )
+})(document);
+
+
+let currentPage = '1';
+
+const PapularDataCategory = () => {
+  return fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+  .then (response => response.json())
+  .then(res => {
+    for(i=0; i<20;i++){
+      (res.results[i])
+    }
+  })
+}
+
+// contruir la categoria popular una vez recibida la info 
+const printPopularHome = ({title, id,  poster_path}, idContainer) => {
+  const ImageContainer = document.getElementById(idContainer);
+    let div = karin('div' ,"imgContainer");
+    
+    let a = karin('a', 'imageLink');  
+    a.href = '#'; 
+
+    let imageResults = karin('img', 'image');
+    imageResults.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+    let titleResults = karin('p', 'imagetitle');
+    titleResults.innerText = title;
+  
+    ImageContainer.appendChild(div);
+    div.appendChild(a);
+    a.appendChild(imageResults);
+    a.appendChild(titleResults); 
+
+  };
+
+ // inicializar  con un onclick en el navegador 
+ //const initialize = () => {
+  //categories.forEach(e => homeData(e))
+//}
+
+
+
+
   // LOAD MORE
 
-  let currentPage = 'currentPage + 1'
+  //let currentPage = 'currentPage + 1'
 
 
   
-// MODAL
+// MODAL  
 
-const modalData = (movieId) => fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
+
+const modalData = (movieId) =>  {
+  fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
   .then(response => response.json())
-  .then(res => console.log(res))
+  .then (data => {
 
-  modalData()
-
-
-
-
-
-const printModal = () => {
    let modal = document.getElementById('miModal');
    let flex = document.getElementById('flex');
-   let span = karin('span', 'close');
-    span.id = 'close';
    let boxes = document.getElementById('open');
    let close = document.getElementById('close');
-
+// crear una sync and await ? para que espere a ejecutarse para cuando le hagamos un click
 
   boxes.addEventListener('click',function(){
       modal.style.display = 'block' ;
@@ -131,27 +190,29 @@ const printModal = () => {
   let modalWrapper = karin('div' ,"contenido-modal");
   //agregar css para que esta imagen sea brackground a la otra imagen?
   let modalBackDropImg = karin('img', 'backdropImage');
-    //modalImage.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+    //modalImage.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${data.poster_path}`;
   let modalHeader = karin('div' ,"modal-headerflex");
   let modalImage = karin('img', 'imageModal');
-    //modalImage.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${backdrop_path}`;
+    //modalImage.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${data.backdrop_path}`;
   let modalTitleWrap = karin('div', 'text');
   let modalTitle = karin('h3');
-  //modalTitle.innerText = title;
+   //modalTitle.innerText = data.title;
   let modalSubTitle = karin('h4');
-  //modalSubTitle.innerText = tagline;
+   modalSubTitle.innerText = data.tagline;
 
   let modalBody = karin('div' ,"modal-body");
   let modalDescription = karin('p' ,"modal-description");
-  //modalDescription.innerText = overview;
+    //modalDescription.innerText = data.overview;
   let modalGenreTitle = karin('h3');
-  //modalGenreTitle.innerText = "GENRES";
+    modalGenreTitle.innerText = "GENRES";
   let modalGenre = karin('p' ,"modal-Genre");
-  //modalGenre.innerText = genres[name];//nose si llega al array con los nombres de los generos;
+
+    modalGenre.innerText = genres[name];//nose si llega al array con los nombres de los generos;
+
   let modalrealeseDateTitle = karin('h3');
-  //modalrealeseDateTitle.innerText = "RELEASE DATE";
+    modalrealeseDateTitle.innerText = "RELEASE DATE";
   let realeseDate = karin('p');
-   //realeseDate.innerText = release_date;
+    //realeseDate.innerText = data.release_date;
 
 
   flex.appendChild(modalWrapper);
@@ -170,13 +231,6 @@ const printModal = () => {
   modalBody.appendChild(modalrealeseDateTitle);
   modalBody.appendChild(realeseDate);
 
-
-
-  
-
-
-
-
+})
 };
-
-printModal()
+ 
--- js/trabaj4.js
+++ js/trabaj4.js
@@ -64,6 +64,63 @@ if(e.target == flex){
 // Apikey
 // 3e597a68977c4cb049bc74d03c2e7ea7
 
+//categoria popular 
+
+
+( function name(d) {
+  let tabs = d.querySelectorAll('.tabs_item');
+  let panels = d.querySelectorAll('.categories_items');
+  d.getElementById('tabs').addEventListener('click', e => {
+    if(e.target.classList.contains ('tabs_item')){
+      let i = (tabs.indexOf(e.target));
+      tabs.map(tab => tab.classList.remove('active'))
+      tabs[i].classList.add('active');
+      panels.map(panel => panel.classList.remove('active'))
+      panels[i].classList.add('active');
+
+    }
+
+  } )
+})(document);
+
+
+let currentPage = '1';
+
+const PapularDataCategory = () => {
+  return fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+  .then (response => response.json())
+  .then(res => {
+    for(i=0; i<20;i++){
+      (res.results[i])
+    }
+  })
+}
+
+// contruir la categoria popular una vez recibida la info 
+const printPopularHome = ({title, id,  poster_path}, idContainer) => {
+  const ImageContainer = document.getElementById(idContainer);
+    let div = karin('div' ,"imgContainer");
+    
+    let a = karin('a', 'imageLink');  
+    a.href = '#'; 
+
+    let imageResults = karin('img', 'image');
+    imageResults.src = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+    let titleResults = karin('p', 'imagetitle');
+    titleResults.innerText = title;
+  
+    ImageContainer.appendChild(div);
+    div.appendChild(a);
+    a.appendChild(imageResults);
+    a.appendChild(titleResults); 
+
+  };
+
+ // inicializar  con un onclick en el navegador 
+ //const initialize = () => {
+  //categories.forEach(e => homeData(e))
+//}
+
 
 
 
